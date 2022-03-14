be60fcd962ef04a2a36b2df0b4732324ec37b051
kartik-gupta-ij <84975264+kartik-gupta-ij@users.noreply.github.com>
2022-03-14 17:34:02 +0530

    new one
    ka

--- assets/js/explorer.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-/*! explorer.js | Friendkit | © Css Ninja. 2019-2020 */$(document).ready((function(){$(".explorer-menu").length&&$("#explorer-trigger, #mobile-explorer-trigger").on("click",(function(){$(".explorer-menu").toggleClass("is-active")}))}));
